d345a606748c5236cbddfe34700f551d9f01b73e
陈志坤 <chenzhikun@17shanyuan.com>
2018-02-02 19:00:50 +0800

    增加中间件学习

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth','CheckAge']], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+
+});
